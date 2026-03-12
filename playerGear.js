85773d168ef254a6c7ec126a80377a2186bd1b76
zachrtermanini-cmyk <zachrtermanini@gmail.com>
2026-03-12 16:12:05 -0400

    Update playerGear.js

--- playerGear.js
+++ playerGear.js
@@ -1341,8 +1341,8 @@ window.playerGear={
             applyStats:function(stats,player){
                 
                 stats.movementCollection+=1000
-                stats.walkSpeed*=2
-                stats.beeSpeed*=2
+                stats.walkSpeed*=3
+                stats.beeSpeed*=3
                
             },
             desc:'Move faster and collect pollen as you walk through flowers!<br><br>+1 movement collection<br>x1.05 bee speed<br>x1.1 movespeed',
